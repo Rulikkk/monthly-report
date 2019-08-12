845d0eccf4d7b384948c6ef50baff3ef1d9757da
Vadim <korobeinikovv@mail.ru>
2019-08-12 22:58:58 +0300

    Improved scrollbars look

---
+++ src/Scrollable/Scrollable.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./styles.css";
+
+export const Scrollable = props => {
+  const { children } = props;
+  return <div className="scrollable__scrollbar">{children}</div>;
+};
