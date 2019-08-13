dc96e86ecb2e04d0120fc9229c796b187a053ce9
Rustem Mustafin <mustafin.rustem@gmail.com>
2019-08-13 14:22:43 +0300

    Update Scrollable.js
    Decided to undo all my commits

--- src/Scrollable/Scrollable.js
+++ src/Scrollable/Scrollable.js
@@ -2,8 +2,6 @@ import React from "react";
 import "./styles.css";
 
 export const Scrollable = props => {
-  const { children } = props,
-    isMacLike = !!navigator.platform.match(/(Mac|iPhone|iPod|iPad)/i);
-  
-  return isMacLike ? children : <div className="scrollable__scrollbar">{children}</div>;
+    const { children } = props;
+	  return <div className="scrollable__scrollbar">{children}</div>;
 };
