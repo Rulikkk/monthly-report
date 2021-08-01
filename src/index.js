22c0bb2fe89d85b064f78f1147b198a77ba63675
Askhat Bikmetov <askhat@hey.com>
2021-08-01 19:17:16 +0300

    Configure recoil

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import ReportAndEditor from "./ReportAndEditor";
+import { RecoilRoot } from "recoil";
 
-const App = () => <ReportAndEditor />;
+const App = () => {
+  return (
+    <Suspense fallback={<span>Loading...</span>}>
+      <RecoilRoot>
+        <ReportAndEditor />
+      </RecoilRoot>
+    </Suspense>
+  );
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
