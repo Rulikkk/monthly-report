4ad1cc6860282659755ed1854410bce96097d8b5
Askhat Bikmetov <askhat@hey.com>
2021-08-05 23:36:22 +0300

    Update api

--- src/MonthlyReport/store/api.js
+++ src/MonthlyReport/store/api.js
@@ -2,7 +2,7 @@ import "../typedef";
 
 import snakeCase from "lodash.snakecase";
 
-import { transformKeys } from "./helpers";
+import { timestamp, transformKeys } from "./helpers";
 import { http } from "./utils";
 
 /**
@@ -11,34 +11,21 @@ import { http } from "./utils";
  * @param {Report} arg document
  * @returns {Promise}
  */
-function push(path, { id, ...rest }) {
+export function push(path, { id, ...rest }) {
+  path = "/" + [path, id].filter(Boolean).join("/");
+  console.log(`${timestamp()} [ push ] ${path}`);
   let payload = transformKeys(rest, snakeCase);
-  return id ? http.put(`/${path}/${id}`, payload) : http.post(`/${path}`, payload);
+  return id ? http.put(path, payload) : http.post(`/${path}`, payload);
 }
 
 /**
- * Create or update report
- * @param {Report} arg
+ * Fetch document
+ * @param {string} path HTTP endpoint
+ * @param {string} id document identifier
  * @returns {Promise}
  */
-export function pushReport(arg) {
-  return push("month", arg);
-}
-
-/**
- * Create or update project status
- * @param {*} arg // :FIXME
- * @returns {Promise}
- */
-export function pushStatus(arg) {
-  return push("status", arg);
-}
-
-/**
- * Create or update project
- * @param {Project} arg
- * @returns {Promise}
- */
-export function pushProject(arg) {
-  return push("project", arg);
+export function pull(path, id) {
+  path = "/" + [path, id].filter(Boolean).join("/");
+  console.log(`${timestamp()} [ pull ] ${path}`);
+  return http.get(path);
 }
