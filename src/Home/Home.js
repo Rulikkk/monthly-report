7f06338a015ec705f20635c79837ff6b825b364f
Alexander <interkst1337@gmail.com>
2021-09-03 12:05:39 +0300

    Home component refactoring

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, {useEffect, useState} from "react"
 import { Link } from "@reach/router";
 import { useRecoilCallback, useRecoilValueLoadable } from "recoil";
 import {
@@ -9,15 +9,9 @@ import {
 
 const Home = () => {
   const ids = useRecoilValueLoadable(allReportsIds);
-
-  let reportLink = "/report/last";
-  let last = null,
-    preLast = null;
-
-  if (ids.state === "hasValue" && ids.contents?.length > 0) {
-    [last, preLast] = ids.contents;
-    reportLink = `/report/${last}`;
-  }
+  const [reportLink, setReportLink] = useState('last');
+  const [lastReportId, setLastReportId] = useState(null);
+  const [preLastReportId, setPreLastReportId] = useState(null);
 
   const preloadReport = useRecoilCallback(({ snapshot }) => (id) => {
     snapshot.getLoadable(reportQuery(id));
@@ -28,12 +22,26 @@ const Home = () => {
   });
 
   useEffect(() => {
-    if (last && preLast) {
-      preloadReport(last);
-      preloadReport(preLast);
-      preloadConfig();
+    preloadConfig();
+  }, []);
+
+  useEffect(() => {
+    if (ids.state === 'hasValue' && Array.isArray(ids.contents) ) {
+      setLastReportId(ids.contents?.[0])
+      setPreLastReportId(ids.contents?.[1])
     }
-  });
+  }, [ids])
+
+  useEffect(() => {
+    if (lastReportId) {
+      setReportLink(lastReportId)
+      preloadReport(lastReportId);
+    }
+  }, [lastReportId])
+
+  useEffect(() => {
+    preLastReportId && preloadReport(preLastReportId);
+  }, [preLastReportId])
 
   return (
     <div
@@ -43,7 +51,7 @@ const Home = () => {
       <img alt="Logo" src="/head.png" className="mx-auto" />
       <nav className="list-disc list-inside m-8">
         <li>
-          <Link to={reportLink} className="underline">
+          <Link to={`/report/${reportLink}`} className="underline">
             Delivery reports
           </Link>
         </li>
